5a838148166861badd35e11d2d4bc91a5395d5e6
SladeGV <32681839+SladeGV@users.noreply.github.com>
2019-05-20 00:41:23 +0100

    Add files via upload

---
+++ java.java
@@ -0,0 +1,21 @@
+public class person{
+	public void setName(){
+
+	}
+}
+public class personA extends person{
+	public void PersonA(){
+		
+	}
+}
+public class personB extends person{
+	public void PersonB(){
+		
+	}
+}
+public class test{
+	public static void main(String[] args){
+		personA a = new personA();
+		personB b = new personB();
+	}
+}
